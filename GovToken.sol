e28c9cf1e2c6863116b1b739251c5dab8c42ca13
Kobita <161524322+kobita1122@users.noreply.github.com>
2026-02-04 13:25:10 +0600

    Create GovToken.sol

---
+++ GovToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+/**
+ * @title GovToken
+ * @dev ERC20 token with voting and delegation capabilities.
+ */
+contract GovToken is ERC20, ERC20Permit, ERC20Votes {
+    constructor() ERC20("GovernanceToken", "GTK") ERC20Permit("GovernanceToken") {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    // Overrides required by Solidity
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
+        return super.nonces(owner);
+    }
+}
